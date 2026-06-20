4504f81e75cf9ae8048a7729ea762b748a0c934f
testnet2049 <www.aragoorn@gmail.com>
2026-06-21 03:44:42 +0400

    Add files via upload
    Add smart contracts

---
+++ Gmonad.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+// Make sure the compiler version is below 0.8.24 since Cancun compiler is not supported just yet
+pragma solidity >=0.8.0 <=0.8.24;
+
+contract Gmonad { 
+    string public greeting;
+
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+    }
+
+    function setGreeting(string calldata _greeting) external {
+        greeting = _greeting;
+    }
+}
